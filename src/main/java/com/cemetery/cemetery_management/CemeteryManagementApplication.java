ba47fb393e5c3be53dba9c12c87678039be47cb3
Soulyrix <alesha.semenov.2006god3fev@mail.ru>
2024-12-16 19:48:05 +0300

    Initial commit

---
+++ src/main/java/com/cemetery/cemetery_management/CemeteryManagementApplication.java
@@ -0,0 +1,13 @@
+package com.cemetery.cemetery_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CemeteryManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CemeteryManagementApplication.class, args);
+	}
+
+}
